a588cfc651c2906d164dc04fb0d83dff749ad104
n3rwus <nerwusik2@vp.pl>
2021-04-02 21:03:05 +0200

    title() method added

--- Command-LineEnglishThesaurus/translate.py
+++ Command-LineEnglishThesaurus/translate.py
@@ -1,12 +1,14 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
+data = json.load(open("Command-LineEnglishThesaurus\data.json"))
 
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %get_close_matches(w, data.keys())[0])
         if yn == "Y":
