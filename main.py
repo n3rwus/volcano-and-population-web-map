e03bcc87e2ff7d18dfe3fb4ae472007306b6f987
n3rwus <nerwusik2@vp.pl>
2021-04-02 19:40:21 +0200

    Simple dist  program made in python.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import cv2 as cv
+import translate as t
 
-img = cv.imread("geeks14.png", cv.IMREAD_COLOR)
 
-cv.imshow("Test", img)
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+
+    word = input("Enter word: ")
+    if type(t.translate(word)) == list:
+        for i in t.translate(word):
+            print(i)
+    else:
+        print(t.translate(word))
