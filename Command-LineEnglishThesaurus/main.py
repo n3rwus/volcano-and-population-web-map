557a22dc01b03aa74adf0bd1160b575099b086c2
n3rwus <nerwusik2@vp.pl>
2021-04-04 00:15:26 +0200

    while loop added

--- Command-LineEnglishThesaurus/main.py
+++ Command-LineEnglishThesaurus/main.py
@@ -1,10 +1,10 @@
 import translate as t
 
 if __name__ == "__main__":
-
-    word = input("Enter word: ")
-    if type(t.translate(word)) == list:
-        for i in t.translate(word):
-            print(i)
-    else:
-        print(t.translate(word))
+    while(input("Would you like to continue?  Enter Y if yes, or press anything to endy: ").lower() == "y"):
+        word = input("Enter word: ")
+        if type(t.translate(word)) == list:
+            for i in t.translate(word):
+                print(i)
+        else:
+            print(t.translate(word))
