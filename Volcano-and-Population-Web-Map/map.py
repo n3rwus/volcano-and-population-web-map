d7017eff30e06b63ff1b692216f71f4566678c9b
n3rwus <nerwusik2@vp.pl>
2021-04-09 16:18:04 +0200

    Color_producer for elev of volcanoes and goole search.

--- Volcano-and-Population-Web-Map/map.py
+++ Volcano-and-Population-Web-Map/map.py
@@ -1,9 +1,36 @@
 import folium
+import pandas
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color_producer(elevation):
+        if elevation < 1000:
+                return "green"
+        elif 1000 <= elevation < 3000:
+                return "orange"
+        else:
+                return "red"
+
+
+html =  """
+        Volcano name:<br>
+        <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+        Height: %s m
+        """
+
+map = folium.Map(location=[40, -102], zoom_start=5, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(clor="green")))
-map.add_child(fg)
 
-map.save("Map.html")
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, int(el)), width=150, height=80)
+    fg.add_child(folium.Marker(location=[lt, ln],
+                               popup=folium.Popup(iframe),
+                               icon=folium.Icon(color=color_producer(el))))
+    
+map.add_child(fg)
+map.save("Map1.html")
